2ecc87acf768a59c8c2e1c298dbe4647c40151be
Kevin DeRudder <kevin_derudder@hotmail.com>
2019-09-17 19:57:47 +0200

    setup

---
+++ songs/js/seed.js
@@ -0,0 +1,22 @@
+window.Seed = (function(){
+    const songs = [
+    {
+        id:1,
+        artist: 'The Roots',
+        title: 'The Seed 2.0',
+        imgSong: './images/theseed.jpg',
+        imgArtist: './images/theroots.jpg',
+        votes: 16
+    },
+    {
+        id:2,
+        artist: 'Queens of the Stone age',
+        title: 'Go with the flow',
+        imgSong: './images/gowiththeflow.png',
+        imgArtist: './images/queens.jpg',
+        votes: 16
+    }
+];
+
+    return {songs: songs};
+})();
